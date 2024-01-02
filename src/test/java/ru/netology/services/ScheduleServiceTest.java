9296010c24945306357b36188e50d15021e02588
Ekaterina Zhmaeva <ekaterina.eduardovn@gmail.com>
2024-01-02 23:47:03 +0300

    Add threshold.csv

--- src/test/java/ru/netology/services/ScheduleServiceTest.java
+++ src/test/java/ru/netology/services/ScheduleServiceTest.java
@@ -1,47 +1,37 @@
 package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class ScheduleServiceTest {
-/*
-        ScheduleService testScheduleCalculate = new ScheduleService();
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/threshold.csv")
 
-        long threshold150000 = testScheduleCalculate.calculate(100_000, 60_000, 150_000);
-        long threshold20000 = testScheduleCalculate.calculate(10_000, 3_000, 20_000);
-        */
-    @Test
-    void calculationOfVacationMonthsWithMoneyThresholdOf20000() {
+    public void calculationOfVacationMonthsWithMoneyThreshold(long income, long expense, long threshold, int expected) {
         ScheduleService service = new ScheduleService();
 
-        //подготовленные данные:
-        long income = 10_000;
-        long expense = 3_000;
-        long threshold = 20_000;
-        int expected = 3;
-
-        //вызываем метод:
         long actual = service.calculate(income, expense, threshold);
 
-        //сравниваем ожидаемый и фактический результат:
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    void calculationOfVacationMonthsWithMoneyThresholdOf150000() {
-        ScheduleService service = new ScheduleService();
+    /*
+     @Test
+     void calculationOfVacationMonthsWithMoneyThresholdOf150000() {
+         ScheduleService service = new ScheduleService();
 
-        //подготовленные данные:
-        long income = 100_000;
-        long expense = 60_000;
-        long threshold = 150_000;
-        int expected = 2;
+         //подготовленные данные:
+         long income = 100_000;
+         long expense = 60_000;
+         long threshold = 150_000;
+         int expected = 2;
 
-        //вызываем метод:
-        long actual = service.calculate(income, expense, threshold);
+         //вызываем метод:
+         long actual = service.calculate(income, expense, threshold);
 
-        //сравниваем ожидаемый и фактический результат:
-        Assertions.assertEquals(expected, actual);
-    }
-
+         //сравниваем ожидаемый и фактический результат:
+         Assertions.assertEquals(expected, actual);
+     }
+    */
 }
